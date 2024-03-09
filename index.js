728342ec522feb8334077f6ee9c4992a139e16de
abhinavanand-fed <abhinavanand080@gmail.com>
2024-03-10 02:37:28 +0530

    minor changes

--- index.js
+++ index.js
@@ -2,14 +2,7 @@ const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, GatewayIntentBits, Events, ActivityType } = require('discord.js');
 const { token } = require('./config.json');
-const { ShardingManager } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-const manager = new ShardingManager('./bot.js', { token: token });
-
-
-manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
-
-manager.spawn();
 
 client.commands = new Collection();
 const foldersPath = path.join(__dirname, 'commands');
@@ -51,39 +44,6 @@ client.on(Events.InteractionCreate, async interaction => {
         return;
     }
 
-    const { cooldowns } = interaction.client;
-
-    if (!cooldowns.has(command.data.name)) {
-        cooldowns.set(command.data.name, new Collection());
-    }
-
-    const now = Date.now();
-    const timestamps = cooldowns.get(command.data.name);
-    const defaultCooldownDuration = 3;
-    const cooldownAmount = (command.cooldown ?? defaultCooldownDuration) * 1000;
-
-    if (timestamps.has(interaction.user.id)) {
-        const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
-
-        if (now < expirationTime) {
-            const expiredTimestamp = Math.round(expirationTime / 1000);
-            return interaction.reply({ content: `Please wait, you are on a cooldown for \`${command.data.name}\`. You can use it again <t:${expiredTimestamp}:R>.`, ephemeral: true });
-        }
-    }
-
-    timestamps.set(interaction.user.id, now);
-    setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
-
-    try {
-        await command.execute(interaction);
-    } catch (error) {
-        console.error(error);
-        if (interaction.replied || interaction.deferred) {
-            await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-        } else {
-            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-        }
-    }
 });
 
 client.on("ready", () => {
