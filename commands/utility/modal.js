f33bc7a2c16a3ad15651a5994d437801d2ee7302
abhinavanand-fed <abhinavanand080@gmail.com>
2024-03-12 15:36:00 +0530

    testing | not working atm

--- commands/utility/modal.js
+++ commands/utility/modal.js
@@ -1,58 +1,72 @@
 const { SlashCommandBuilder } = require('discord.js');
-const { ActionRowBuilder, ButtonBuilder, ButtonStyle, ModalBuilder } = require('discord.js');
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle, ModalBuilder, TextInputBuilder, TextInputStyle } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('report')
         .setDescription('Report a user.'),
-        async execute(interaction) {
-            const userId = interaction.options.getString('user-id');
-            const username = interaction.options.getString('username');
-            const link = interaction.options.getString('link');
-        
-            // Send the report button to the specified channel
-            const reportChannel = interaction.client.channels.cache.get('1189856644805443635');
-            const responseChannel = interaction.client.channels.cache.get('1189856644805443635'); // Change CHANNEL_ID_HERE to your desired channel ID
-        
-            if (reportChannel && responseChannel) {
-                const row = new ActionRowBuilder()
-                    .addComponents(
-                        new ButtonBuilder()
-                            .setCustomId('submit_report')
-                            .setLabel('Submit Report')
-                            .setStyle(ButtonStyle.Primary)
-                    );
-        
-                await reportChannel.send({ content: `ID: ${userId}\nUsername: ${username}\nLink: ${link}\n---`, components: [row] });
-        
-                const collectorFilter = i => i.isButton() && i.customId === 'submit_report';
-                const collector = interaction.channel.createMessageCollector({ filter: collectorFilter, time: 15_000 });
-        
-                collector.on('collect', async i => {
-                    const modal = new ModalBuilder()
-                        .setTitle('Modal Title')
-                        .setDescription('Modal Description')
-                        .setSubmitButton('Submit')
-                        .setCancelButton('Cancel');
-        
-                    await i.reply({ content: 'Please provide additional information in the modal.', components: [], embeds: [modal] });
-        
-                    const modalCollectorFilter = m => m.author.id === interaction.user.id;
-                    const modalCollector = responseChannel.createMessageCollector({ filter: modalCollectorFilter, time: 30000 });
-        
-                    modalCollector.on('collect', async m => {
-                        await interaction.followUp(`Report submitted. Additional information: ${m.content}`);
-                        modalCollector.stop();
-                    });
-        
-                    modalCollector.on('end', collected => {
-                        if (collected.size === 0) {
-                            interaction.followUp('Report timed out.');
-                        }
-                    });
-                });
-            } else {
-                interaction.reply({ content: 'Error: Report channel not found.', ephemeral: true });
-            }
-        },
-    };
+    async execute(interaction) {
+        const row = new ActionRowBuilder()
+            .addComponents(
+                new ButtonBuilder()
+                    .setCustomId('submit_report')
+                    .setLabel('Submit Report')
+                    .setStyle(ButtonStyle.Primary)
+            );
+
+        await interaction.reply({ content: 'Tap here to report', components: [row] });
+
+        const collectorFilter = i => i.isButton() && i.customId === 'submit_report';
+        const collector = interaction.channel.createMessageComponentCollector({ filter: collectorFilter, time: 15000 });
+
+        collector.on('collect', async i => {
+            const modal = new ModalBuilder()
+                .setTitle('Report User') // Ensure 'Report User' is a string
+                .addField('Please provide the following information:', [
+                    new TextInputBuilder()
+                        .setCustomId('user-id')
+                        .setLabel('User ID')
+                        .setStyle(TextInputStyle.Short)
+                        .setRequired(true),
+                    new TextInputBuilder()
+                        .setCustomId('username')
+                        .setLabel('Username')
+                        .setStyle(TextInputStyle.Short)
+                        .setRequired(true),
+                    new TextInputBuilder()
+                        .setCustomId('link')
+                        .setLabel('Evidence Link')
+                        .setStyle(TextInputStyle.Short)
+                        .setRequired(true)
+                ]);
+
+
+            modal.addComponents(userIdInput, usernameInput, linkInput);
+
+            await i.reply({ content: 'Please provide the following information:', components: [], embeds: [modal] });
+
+            const modalCollectorFilter = m => m.isMessageComponent() && m.customId === 'submit_report';
+            const modalCollector = interaction.channel.createMessageComponentCollector({ filter: modalCollectorFilter, time: 30000 });
+
+            modalCollector.on('collect', async m => {
+                const userId = m.values['user-id'];
+                const username = m.values['username'];
+                const link = m.values['link'];
+
+                const reportChannel = interaction.client.channels.cache.get('1189856644805443634');
+                if (reportChannel) {
+                    await reportChannel.send(`ID: ${userId}\nUsername: ${username}\nLink: ${link}`);
+                }
+
+                await interaction.followUp('Report submitted.');
+                modalCollector.stop();
+            });
+
+            modalCollector.on('end', collected => {
+                if (collected.size === 0) {
+                    interaction.followUp('Report timed out.');
+                }
+            });
+        });
+    },
+};
