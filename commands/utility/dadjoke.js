ef1e6fc096a0c09d5aa00bdb44068b06213ec9d2
abhinavanand-fed <abhinavanand080@gmail.com>
2024-03-15 17:34:57 +0530

    dadjoke

---
+++ commands/utility/dadjoke.js
@@ -0,0 +1,36 @@
+const {
+    SlashCommandBuilder,
+    ChatInputCommandInteraction,
+    EmbedBuilder,
+  } = require('discord.js');
+  const axios = require('axios');
+  
+  module.exports = {
+    cooldown: 10,
+    data: new SlashCommandBuilder()
+      .setName('dadjokes')
+      .setDescription('Random dad jokes from icanhazdadjoke.com')
+      .setDMPermission(false),
+    /**
+     *
+     * @param {ChatInputCommandInteraction} interaction
+     * @param {Client} client
+     */
+    async execute(interaction, client) {
+      try {
+        await interaction.deferReply();
+  
+        const response = await axios.get('https://icanhazdadjoke.com/slack');
+        const joke = response.data.attachments[0].text;
+  
+        const embed = new EmbedBuilder()
+          .setColor('Random')
+          .setFooter({ text: 'Dad jokes - icanhazdadjoke.com' })
+          .setDescription(joke);
+  
+        await interaction.editReply({ embeds: [embed] });
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  };
