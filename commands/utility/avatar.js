61f9d6d5560508a0f1fd5cbc81aabfe69ba341f0
abhinavanand-fed <abhinavanand080@gmail.com>
2024-03-15 17:34:41 +0530

    avi fetch

---
+++ commands/utility/avatar.js
@@ -0,0 +1,39 @@
+// Shows the user's avatar
+const {
+    ChatInputCommandInteraction,
+    SlashCommandBuilder,
+    EmbedBuilder,
+    Client,
+  } = require('discord.js');
+  
+  module.exports = {
+    data: new SlashCommandBuilder()
+      .setName('avatar')
+      .setDescription(`Shows a user's avatar`)
+      .setDMPermission(false)
+      .addUserOption(option =>
+        option.setName('input').setDescription('Select the user')
+      ),
+    /**
+     *
+     * @param {ChatInputCommandInteraction} interaction
+     * @param {Client} client
+     */
+    async execute(interaction) {
+      const target = interaction.options.getMember('input') || interaction.member;
+      const { user } = target;
+  
+      const newEmbed = new EmbedBuilder()
+        .setAuthor({
+          name: `${user.tag}`,
+          iconURL: target.displayAvatarURL(),
+        })
+        .setTitle(`User Avatar`)
+        .setImage(user.displayAvatarURL({ size: 4096 }))
+        .setTimestamp();
+  
+      await interaction.reply({
+        embeds: [newEmbed],
+      });
+    },
+  };
