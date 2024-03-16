91f8f2cdc30b1efb985b6f55035616d84fc1261a
abhinavanand-fed <abhinavanand080@gmail.com>
2024-03-16 19:00:55 +0530

    removed partials

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const { Collection, GatewayIntentBits, Events, ActivityType, Client, Partials } = require('discord.js');
+const { Collection, GatewayIntentBits, Events, ActivityType, Client } = require('discord.js');
 const { token } = require('./config.json');
 
 const client = new Client({
