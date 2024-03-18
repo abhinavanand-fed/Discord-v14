21dd03eb5d05021b2d25f90637ea9c9a1c4d711e
abhinavanand-fed <abhinavanand080@gmail.com>
2024-03-18 19:54:01 +0530

    updated ban.js

--- commands/utility/ban.js
+++ commands/utility/ban.js
@@ -25,7 +25,7 @@ module.exports = {
                 .setRequired(false)
         ),
 
-    run: async ({ interaction }) => {
+    async execute(interaction) {
         const { options, member } = interaction;
 
         const target = options.getMember('target');
